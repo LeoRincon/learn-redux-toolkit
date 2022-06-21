e0b3709d50167ecdc76b931827fabad17fa4c124
Leonardo Rincon <jleobotache@gmail.com>
2022-06-21 15:48:21 -0500

    create the store to pokemonApp

---
+++ src/Pokemon.jsx
@@ -0,0 +1,14 @@
+export const Pokemon = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+        <li>four</li>
+      </ul>
+    </>
+  );
+};
---
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState: {
+    page: 0,
+    pokemons: [],
+    isLoading: false,
+  },
+  reducers: {
+    increment: (state /* action */) => {},
+  },
+});
+export const { increment } = pokemonSlice.actions;
