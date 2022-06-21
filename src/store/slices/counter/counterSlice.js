fe73fff9e1b1bedefed86390e55969e878687045
Leonardo Rincon <jleobotache@gmail.com>
2022-06-21 14:23:06 -0500

    create un counter with redux

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -9,7 +9,13 @@ export const counterSlice = createSlice({
     increment: (state) => {
       state.counter += 1;
     },
+    decrement: (state) => {
+      state.counter -= 1;
+    },
+    incrementBy: (state, action) => {
+      state.counter += action.payload;
+    },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementBy } = counterSlice.actions;
